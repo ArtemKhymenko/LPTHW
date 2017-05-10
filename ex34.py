e28ca813350278d3378640026a7d7713b7f8f900
Artem Khymenko <breakerhim@ua.fm>
2017-05-10 22:25:12 +0300

    Exercise 34

---
+++ ex34.py
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+"The animal at first is 2nd animal and is python"
+"The third animal (3rd) is at 2 and is a peacock"
+"The first (1st) animal is at 0 and is a bear"
+"The animal at 3 is the 4th animal and a kangaroo"
+"The fifth (5th) animal is at 4 and is a whale"
+"The animal at 2 is the 3th animal and is a peacock"
+"The sixth (6th) animal is at 5 and is a platypus"
+"The animal at 4 is the 5th and is a whale"
